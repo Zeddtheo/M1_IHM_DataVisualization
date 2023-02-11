9c8eddae7084664377d2c26e06747d513425a5c5
MISC <50262380+Zeddtheo@users.noreply.github.com>
2023-02-11 01:17:12 +0100

    1st version

---
+++ edge.h
@@ -0,0 +1,100 @@
+#ifndef EDGE_H
+#define EDGE_H
+
+#include <QGraphicsItem>
+
+class Node;
+
+/**
+ * @brief La classe Edge représente une arête dans un graphe.
+ *
+ * Elle est dérivée de QGraphicsItem et est responsable de dessiner une arête entre deux nœuds dans un graphe.
+ * Elle stocke les informations sur les nœuds source et destination, la couleur et la taille de l'arête.
+ * La méthode adjust() est utilisée pour ajuster la position des nœuds source et destination sur l'arête.
+ * La méthode paint() est utilisée pour dessiner l'arête en utilisant les informations stockées.
+
+ */
+class Edge : public QGraphicsItem
+{
+public:
+
+    /**
+     * @brief Constructeur pour la classe Edge.
+     * @param sourceNode Le nœud source pour l'arête.
+     * @param destNode Le nœud de destination pour l'arête.
+     * @param color La couleur de l'arête.
+     * @param edgeSize La taille de l'arête.
+     */
+    Edge(Node *sourceNode, Node *destNode,QColor color,int edgeSize);
+
+    /**
+     * @brief Renvoie le nœud source de l'arête.
+     * @return Le nœud source.
+     */
+    Node *sourceNode() const;
+
+    /**
+     * @brief Renvoie le nœud de destination de l'arête.
+     * @return Le nœud de destination.
+     */
+    Node *destNode() const;
+
+    /**
+     * @brief Ajuste la position des nœuds source et destination sur l'arête.
+     *
+     * Cette méthode calcule la position des nœuds source et destination sur l'arête en fonction de la position de chaque nœud sur le graphe.
+     */
+    void adjust();
+
+    enum { Type = UserType + 2 };
+    int type() const override { return Type; }
+
+    /**
+     * @brief Est ce que le noeud est selectionné
+     */
+    bool isSelected;
+
+    /**
+     * @brief Modifier la couleur du noeud
+     * @param La couleur que l'on veut mettre
+     */
+    void setColor(QColor color);
+protected:
+
+    /**
+     * @brief Renvoie la zone englobante pour l'arête.
+     * @return La zone englobante.
+     */
+    QRectF boundingRect() const override;
+
+    /**
+     * @brief  Dessine l'arête.
+     *
+     * @param painter Le peintre utilisé pour dessiner l'arête.
+     * @param option Les options de style pour dessiner l'arête.
+     * @param widget Le widget parent pour dessiner l'arête.
+     *
+     * Cette méthode utilise les informations stockées
+     */
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget) override;
+
+    /**
+     * @brief Est déclenché quand on clique dessus et applique soit le mouvement si clique gauche soit la sélection si clique droit
+     * @param event
+     */
+    void mousePressEvent(QGraphicsSceneMouseEvent *event);
+
+private:
+    Node *source, *dest;
+    QColor color;
+    QColor colorUser;
+    int edgeSize;
+
+    QPointF sourcePoint;
+    QPointF destPoint;
+    qreal arrowSize = 10;
+
+
+};
+
+#endif // EDGE_H
